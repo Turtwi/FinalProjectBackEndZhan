7958f3d60ac74d1a2e3a8f182347935a59e1ac55
Turtwi <210103367@stu.sdu.edu.kz>
2023-05-13 18:21:13 +0600

    BackEnd

---
+++ FinalBack/FinalApp/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Post, Comment
+
+
+class CreateUser(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class CreatePost(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['photo', 'text']
+
+
+class WriteComment(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
---
+++ FinalBack/FinalApp/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='static/images')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
